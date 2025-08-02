0707f3f20ca817611923576da3bbe1c8f24680f1
wnj719 <fina79513@gmail.com>
2025-08-02 18:55:33 +0900

    Light

--- Assets/02.Scripts/SunLightController.cs
+++ Assets/02.Scripts/SunLightController.cs
@@ -6,10 +6,11 @@ public class SunLightController : MonoBehaviour
 {
     [SerializeField] private Transform targetOrigin; // 회전 기준 위치 (예: 화면 중심)
 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.transform.GetChild(0).gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -17,7 +18,19 @@ public class SunLightController : MonoBehaviour
     {
         if (Camera.main == null) return;
 
-        // 마우스 위치를 월드 좌표로 변환
+        if (Input.GetMouseButtonDown(0)) // 0: 좌클릭, 1: 우클릭, 2: 휠클릭
+        {
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            Debug.Log("누른 상태");
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        }
+
+  
+
+            // 마우스 위치를 월드 좌표로 변환
         Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mouseWorld.z = 0f;
 
@@ -31,7 +44,7 @@ public class SunLightController : MonoBehaviour
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 
         // Z축 기준 회전 적용 (2D)
-        transform.rotation = Quaternion.Euler(0, 0, angle);
+        transform.rotation = Quaternion.Euler(0, 0, angle+90f);
 
     }
 }
