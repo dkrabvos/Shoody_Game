7e16b924613d64727f097fea12d3cfb1658069b1
wnj719 <fina79513@gmail.com>
2025-08-02 21:43:00 +0900

    Create PlayerHealth.cs

---
+++ Assets/02.Scripts/PlayerHealth.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHP = 100;
+    public float currentHP;
+    public Slider healthSlider;
+
+    public float maxHealth = 100f;
+    public float currentHealth;
+    void Start()
+    {
+        currentHP = maxHP;
+
+        if (healthSlider != null)
+        {
+            healthSlider.maxValue = maxHP;
+            healthSlider.value = currentHP;
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHP -= amount;
+        currentHP = Mathf.Max(currentHP, 0);
+
+        Debug.Log("플레이어 체력: " + currentHP);
+
+        if (healthSlider != null)
+        {
+            healthSlider.value = currentHP;
+        }
+
+        if (currentHP <= 0)
+        {
+            Debug.Log("플레이어 사망!");
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        currentHP = Mathf.Min(currentHP + amount, maxHP);
+        Debug.Log($"[Sun] 회복됨! 현재 체력: {currentHP}");
+    }
+}
